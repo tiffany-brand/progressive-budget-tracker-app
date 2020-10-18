0e75de998eafbcae1912e532ad98cf388f6da8f1
Tiffany Brand <brand2learn@gmail.com>
2020-10-18 15:46:32 -0400

    Add dotenv and MongoDB Atlas connection

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+require('dotenv').config()
+
 const PORT = 3000;
 
 const app = express();
@@ -15,10 +17,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// connect to Mongo database with Mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
-});
+}
+);
+
 
 // routes
 app.use(require("./routes/api.js"));
